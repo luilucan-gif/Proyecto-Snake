61a8e64fd6b010a35702ce516a8183e0955297d8
Luis Lucas <luilucan@gmail.com>
2026-04-10 01:26:27 -0500

    Inicio del proyecto con input y print

---
+++ juego.py
@@ -0,0 +1,5 @@
+# Proyecto Snake - avance inicial
+# Se solicita el nombre del jugador y se muestra en pantalla
+
+jugador = input("Ingrese el nombre del jugador: ")
+print("Bienvenido al juego Snake,", jugador)
